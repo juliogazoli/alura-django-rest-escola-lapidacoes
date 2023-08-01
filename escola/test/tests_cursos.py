613f217463b022e2d26b8574e3f4b2204a61ca1a
juliogazoli <julio_gazoli@hotmail.com>
2023-08-01 12:03:34 -0300

    Testando PUT e DELETE

--- escola/test/tests_cursos.py
+++ escola/test/tests_cursos.py
@@ -32,3 +32,18 @@ class CursosTestCase(APITestCase):
         }
         response = self.client.post(self.list_url, data=data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_requisicao_delete_para_deletar_curso(self):
+        """Teste para verificar a requisição DELETE não permitida para deletar um curso"""
+        response = self.client.delete('/cursos/1/')
+        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_requisicao_put_para_atualizar_curso(self):
+        """Teste para verificar requisição PUT para atualizar um curso"""
+        data = {
+            'codigo_curso':'CTT1',
+            'descricao':'Curso teste 1 atualizado',
+            'nivel':'I'
+        }
+        respone = self.client.put('/cursos/1/', data)
+        self.assertEquals(respone.status_code, status.HTTP_200_OK)
